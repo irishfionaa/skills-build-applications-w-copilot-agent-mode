d08d079861dc391aa4fa002d918f1585c3f0a450
FionaA <38359189+irishfionaa@users.noreply.github.com>
2025-04-08 18:03:06 +0000

    commit

--- octofit-tracker/backend/octofit_tracker/management/commands/populate_db.py
+++ octofit-tracker/backend/octofit_tracker/management/commands/populate_db.py
@@ -6,7 +6,7 @@ from datetime import timedelta
 from bson import ObjectId
 
 class Command(BaseCommand):
-    help = 'Populate the database with test data for users, teams, activity, leaderboard, and workouts'
+    help = 'Populate the database with test data for users, teams, activities, leaderboard, and workouts'
 
     def handle(self, *args, **kwargs):
         # Connect to MongoDB
@@ -25,7 +25,7 @@ class Command(BaseCommand):
             User(_id=ObjectId(), username='thundergod', email='thundergod@mhigh.edu', password='thundergodpassword'),
             User(_id=ObjectId(), username='metalgeek', email='metalgeek@mhigh.edu', password='metalgeekpassword'),
             User(_id=ObjectId(), username='zerocool', email='zerocool@mhigh.edu', password='zerocoolpassword'),
-            User(_id=ObjectId(), username='crashoverride', email='crashoverride@hmhigh.edu', password='crashoverridepassword'),
+            User(_id=ObjectId(), username='crashoverride', email='crashoverride@mhigh.edu', password='crashoverridepassword'),
             User(_id=ObjectId(), username='sleeptoken', email='sleeptoken@mhigh.edu', password='sleeptokenpassword'),
         ]
         User.objects.bulk_create(users)
@@ -35,10 +35,8 @@ class Command(BaseCommand):
         team2 = Team(_id=ObjectId(), name='Gold Team')
         team1.save()
         team2.save()
-        for user in users[:3]:
+        for user in users:
             team1.members.add(user)
-        for user in users[3:]:
-            team2.members.add(user)
 
         # Create activities
         activities = [
